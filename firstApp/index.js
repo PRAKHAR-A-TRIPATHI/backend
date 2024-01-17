6ab787f442e6654e48bb03149d55e7964d975de3
PRAKHAR-A-TRIPATHI <prakhar.tripathi@saeculumsolutions.com>
2024-01-17 18:41:51 +0530

    update

---
+++ firstApp/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send("Prakhar Tripathi")
+})
+app.get('/login', (req, res) => {
+    res.send("<h1>login<h1>")
+})
+app.get('/new', (req, res) => {
+    res.send("<h1>padhle backend<h1>")
+})
+
+app.listen(process.env, () => {
+    console.log(`Example app listening on port ${process.env}`)
+})
