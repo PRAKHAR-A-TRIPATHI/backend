80999cc06e4956b37c3fd2254beb79e5b6cf6050
PRAKHAR-A-TRIPATHI <prakhar.tripathi@saeculumsolutions.com>
2024-01-18 17:30:07 +0530

    new

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -19,5 +18,5 @@ app.get('/new', (req, res) => {
 })
 
 app.listen(process.env, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port`)
 })
