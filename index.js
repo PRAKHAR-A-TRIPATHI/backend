ab0507cfb8b80b1f1e0c723fe42c489282ebedd2
PRAKHAR-A-TRIPATHI <prakhar.tripathi@saeculumsolutions.com>
2024-01-18 17:51:50 +0530

    update.js

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000;
+const githubData = {
+    "a":1,
+    "k":3,
+    "s":5,
+    "e":6,
+    "d":7,
+    "c":8,
+    "b":9,
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -16,7 +26,10 @@ app.get('/login', (req, res) => {
 app.get('/new', (req, res) => {
     res.send("<h1> backend<h1>")
 })
-
-app.listen(process.env, () => {
-    console.log(`Example app listening on port`)
+app.get("/github",(req,res)=>{
+    res.json(githubData)
 })
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
