5f79a239271259d055dba4fc08b656cfae1a57a8
PRAKHAR-A-TRIPATHI <prakhar.tripathi@saeculumsolutions.com>
2024-01-18 17:56:01 +0530

    dataUpdate

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 const githubData = {
-    "a":1,
-    "k":3,
-    "s":5,
-    "e":6,
-    "d":7,
-    "c":8,
-    "b":9,
+    "a": 1,
+    "k": 3,
+    "s": 5,
+    "e": 6,
+    "d": 7,
+    "c": 8,
+    "b": 9,
+}
+const data = {
+    "id": "abc123",
+    "name": "AAA 000",
+    "imei": "123456789123",
+    "vehicle_params": {
+        "vin": null,
+        "make": null,
+        "model": null,
+          "plate_number": null
+    }
 }
 
 app.get('/', (req, res) => {
@@ -26,10 +37,13 @@ app.get('/login', (req, res) => {
 app.get('/new', (req, res) => {
     res.send("<h1> backend<h1>")
 })
-app.get("/github",(req,res)=>{
+app.get("/github", (req, res) => {
     res.json(githubData)
+})
+app.get("/data", (req, res) => {
+    res.json(data)
 })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-  });
+});
